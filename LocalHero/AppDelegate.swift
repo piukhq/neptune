abe8d9971216322130985d625696af3c640a6556
Sean Williams <swilliams@bink.com>
2022-02-18 14:53:06 +0000

    Add and configure firebase

--- LocalHero/AppDelegate.swift
+++ LocalHero/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Sean Williams on 16/02/2022.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
