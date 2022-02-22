52580e8ffc01cc6c1bb49c0148cb0523ce97e39b
Sean Williams <swilliams@bink.com>
2022-02-22 14:40:19 +0000

    Create login vc, root state machine and world class. Remove scene delegate

---
+++ LocalHero/World.swift
@@ -0,0 +1,15 @@
+//
+//  World.swift
+//  LocalHero
+//
+//  Created by Sean Williams on 22/02/2022.
+//
+
+import Foundation
+
+let Current = World()
+
+class World {
+    lazy var rootStateMachine = RootStateMachine()
+    
+}
