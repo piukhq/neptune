2eda8540a02d009b5fe0e5a82be829981c5b8eb0
Sean Williams <swilliams@bink.com>
2022-02-22 14:56:38 +0000

    Add button to login screen

--- LocalHero/Screens/LoginViewController.swift
+++ LocalHero/Screens/LoginViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .white
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        button.layer.cornerRadius = 25
+        button.layer.cornerCurve = .continuous
+        view.addSubview(button)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .purple
+        
+        NSLayoutConstraint.activate([
+            loginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc private func loginButtonTapped() {
+        print("Tapped")
     }
 }
 
